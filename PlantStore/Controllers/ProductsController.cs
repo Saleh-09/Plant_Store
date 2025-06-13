e2a84d1b7a286ae22c508c30ad0fc6117bc766a5
Saleh <saleharain03@gmail.com>
2025-06-13 12:20:31 +0500

    Product Controller is Added

---
+++ PlantStore/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Core.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using PlantStore.DataAccess.Data;
+using PlantStore.DTOs;
+namespace PlantStore.Controllers
+
+{
+    public class ProductsController : Controller
+    {
+        private readonly StoreDbContext _context;
+        public ProductsController(StoreDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(produ product)
+        {
+            
+        }
+    }
+}
