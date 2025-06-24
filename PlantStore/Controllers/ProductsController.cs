affc6c9f76e809f996d48995c6d8a2922819d701
Saleh <saleharain03@gmail.com>
2025-06-24 10:19:21 +0500

    Added Products Controller

--- PlantStore/Controllers/ProductsController.cs
+++ PlantStore/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlantStore.DataAccess.Data;
 using PlantStore.DTOs;
+using PlantStore.Models.Models;
 namespace PlantStore.Controllers
 
 {
@@ -12,15 +13,49 @@ namespace PlantStore.Controllers
         {
             this._context = context;
         }
+        public IActionResult ProductsList()
+        {
+            var products = _context.Products
+                .Select(p => new ProductDisplayViewModel
+                {
+                    Name = p.Name,
+                    Description = p.Description,
+                    Picture = p.Picture,
+                    PictureFormat = $"data:image/png;base64,{Convert.ToBase64String(p.Picture)}",
+                    Price = p.Price,
+                    Stock = p.Stock
+                }).ToList();
+
+            return View(products);
+        }
         [HttpGet]
         public IActionResult AddProduct()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult AddProduct(produ product)
+        public IActionResult Create(ProductViewModel model)
         {
-            
+            if (!ModelState.IsValid)
+                return View(model);
+
+            using var ms = new MemoryStream();
+            model.Picture.CopyTo(ms);
+
+            var product = new Product
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Picture = ms.ToArray(),
+                Price = model.Price,
+                Stock = model.Stock
+            };
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            ModelState.Clear();
+
+            return RedirectToAction("ProductsList");
         }
     }
 }
