dc3c1ccbcb5fab7f29239cfca974b3cc32edaf45
Saleh <saleharain03@gmail.com>
2025-07-01 13:25:25 +0500

    Added CSS Styling

--- PlantStore/Program.cs
+++ PlantStore/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Products}/{action=ProductsList}/{id?}");
+    pattern: "{controller=Products}/{action=ProductList}/{id?}");
 
 app.Run();
