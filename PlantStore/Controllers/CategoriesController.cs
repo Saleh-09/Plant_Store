559039bf39e2dc3b88b905c5473a7d5047d198f0
Saleh <saleharain03@gmail.com>
2025-07-01 13:23:32 +0500

    Added Category Flow

---
+++ PlantStore/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlantStore.DataAccess.Data;
+using PlantStore.DTOs;
+using PlantStore.Models.Models;
+
+namespace PlantStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly StoreDbContext _context;
+        public CategoriesController(StoreDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(CategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var category = new Category
+            {
+                Name = model.Name
+            };
+
+            ModelState.Clear();
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("AddCategory");
+        }
+    }
+}
---
+++ PlantStore/DTOs/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlantStore.DTOs
+{
+    public class CategoryViewModel
+    {
+        [Required(ErrorMessage = "Category ID is required.")]
+        public string Name { get; set; } = default!;
+    }
+}
