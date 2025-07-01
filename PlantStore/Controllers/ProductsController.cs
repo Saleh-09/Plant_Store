2f9c7775e1788f0168d6fbdf8b6e105ff8a739ea
Saleh <saleharain03@gmail.com>
2025-07-01 13:25:01 +0500

    Modified Products Flow

--- PlantStore/Controllers/ProductsController.cs
+++ PlantStore/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Core.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PlantStore.DataAccess.Data;
 using PlantStore.DTOs;
 using PlantStore.Models.Models;
@@ -13,9 +14,23 @@ namespace PlantStore.Controllers
         {
             this._context = context;
         }
-        public IActionResult ProductsList()
+        public IActionResult ProductList(int? categoryId)
         {
-            var products = _context.Products
+            var categories = _context.Categories
+         .Select(c => new SelectListItem
+         {
+             Value = c.CategoryId.ToString(),
+             Text = c.Name
+         }).ToList();
+
+            var productsQuery = _context.Products.AsQueryable();
+
+            if (categoryId.HasValue && categoryId.Value > 0)
+            {
+                productsQuery = productsQuery.Where(p => p.CategoryId == categoryId.Value);
+            }
+
+            var products = productsQuery
                 .Select(p => new ProductDisplayViewModel
                 {
                     Name = p.Name,
@@ -26,36 +41,61 @@ namespace PlantStore.Controllers
                     Stock = p.Stock
                 }).ToList();
 
+            ViewBag.Categories = categories;
+            ViewBag.SelectedCategory = categoryId ?? 0;
+
             return View(products);
         }
+  
         [HttpGet]
         public IActionResult AddProduct()
         {
-            return View();
+            var model = new ProductViewModel
+            {
+                Categories = _context.Categories
+                    .Select(c => new SelectListItem
+                    {
+                        Value = c.CategoryId.ToString(),
+                        Text = c.Name
+                    }).ToList()
+            };
+            return View(model);
         }
+
         [HttpPost]
-        public IActionResult Create(ProductViewModel model)
+        public IActionResult AddProduct(ProductViewModel model)
         {
             if (!ModelState.IsValid)
+            {
+                // Repopulate categories if validation fails
+                model.Categories = _context.Categories
+                    .Select(c => new SelectListItem
+                    {
+                        Value = c.CategoryId.ToString(),
+                        Text = c.Name
+                    }).ToList();
                 return View(model);
+            }
 
             using var ms = new MemoryStream();
             model.Picture.CopyTo(ms);
-
+            
             var product = new Product
             {
                 Name = model.Name,
                 Description = model.Description,
                 Picture = ms.ToArray(),
+                PictureFormat = model.Picture.ContentType,
                 Price = model.Price,
-                Stock = model.Stock
+                Stock = model.Stock,
+                CategoryId = model.CategoryId
             };
 
             _context.Products.Add(product);
             _context.SaveChanges();
             ModelState.Clear();
 
-            return RedirectToAction("ProductsList");
+            return RedirectToAction("ProductList");
         }
     }
 }
--- PlantStore/DTOs/ProductViewModel.cs
+++ PlantStore/DTOs/ProductViewModel.cs
@@ -1,13 +1,16 @@
-﻿using PlantStore.Models.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using PlantStore.Models.Models;
 
 namespace PlantStore.DTOs
 {
     public class ProductViewModel
     {
-        public string Name { get; set; } = default!;
-        public string Description { get; set; } = default!;
+        public string Name { get; set; }
+        public string Description { get; set; }
         public IFormFile Picture { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
     }
 }
